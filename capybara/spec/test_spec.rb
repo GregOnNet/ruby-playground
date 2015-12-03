94fc336c945092d0cf3984147eebbbe08699920f
Gregor Woiwode <gw@co-it.eu>
2015-12-03 14:29:46 +0100

    Adds sample to run ui tests with capybara

---
+++ capybara/spec/test_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require 'capybara'
+require 'capybara/dsl'
+
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://www.youtube.com'
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
+
+describe 'Searching for a video' do
+  it 'allows searches for general terms' do
+    visit ''
+    fill_in('search_query', with: 'text adventures')
+    click_button('search-btn')
+    page.should have_content('GET LAMP: The Text Adventure Documentary')
+  end
+end
